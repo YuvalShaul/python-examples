17932a8ff758a74d663bad1fa9a52c40c442c9c8
Yuval Shaul <yuval.shaul@gmail.com>
2022-08-19 04:36:11 -0400

    add scope examples

---
+++ functions/scope.py
@@ -0,0 +1,16 @@
+
+
+from hashlib import new
+
+
+my_num = 5
+
+def change_num(new_value):
+    global my_num 
+    my_num = new_value
+    pass
+
+
+change_num(7)
+print(my_num)
+
---
+++ functions/scope2.py
@@ -0,0 +1,18 @@
+
+v1 = 5
+
+def outer():
+    v1 = 6
+    def inner():
+        # choose and see the differences:
+        # global v1
+        nonlocal v1
+        v1 = 7
+    inner()
+    print("v1 outer:", v1)
+    
+
+outer()
+
+print('v1 global:', v1)
+
